79c63f1a6657adeee436a12df3fc15ed4cc6c9ea
KarikariSamuelZachary <skarikaripresecghana@gmail.com>
2024-01-09 23:33:25 +0100

    Correct

---
+++ HackerRank/commands.py
@@ -0,0 +1,26 @@
+if __name__ == '__main__':
+    N = int(input())
+    
+    list = []
+    for _ in range(N):
+        command = input().split()
+        
+        if command[0] == 'insert':
+            i, e = map(int, command[1:])
+            list.insert(i, e)
+        elif command[0] == 'print':
+            print(list)
+        elif command[0] == 'remove':
+            e = int(command[1])
+            list.remove(e)
+        elif command[0] == 'append':
+            e = int(command[1])
+            list.append(e)
+        elif command[0] == 'sort':
+            list.sort()
+        elif command[0] == 'pop':
+            list.pop()
+        elif command[0] == 'reverse':
+            list.reverse()
+            
+            
